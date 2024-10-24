8381e1e3b4a4c0efc911441f3b4f7cef2b623ffc
Faisal Hameed <faisalh374@gmail.com>
2024-10-24 09:58:05 +0500

    ok

--- src/app/Country/page.tsx
+++ src/app/Country/page.tsx
@@ -4,25 +4,24 @@ import React from 'react'
 function Country () {
   return (
     <div className='m-4 text-center m-b2'>
-      
-<<<<<<< HEAD
-<h1 className=' text-white-2x1 border-2 p-2 bg-purple-400 m-4'>List of countries</h1>
-=======
+
+  <h1 className=' text-white-2x1 border-2 p-2 bg-purple-400 m-4'>List of countries</h1>
+
 <h1 className='text-white-2x1 border-2 p-2 bg-purple-400 m-4'>List of countries</h1>
->>>>>>> 2ce83dd7ee14b14bf2b37e4d2d7382907ecd44d4
+
 <Navbar href="/Country/pakistan"name ='pakistan' />
 <Navbar href='/Country/india'name='india'/>
 <Navbar href='/Country/sudiarab'name='sudiarab'/>
 <Navbar href='/Country/england' name='england '/>
-<<<<<<< HEAD
 <Navbar href='/Country/Iran'name='Iran'/>
-=======
-<Navbar href='/Country/iran'name='iran'/>
->>>>>>> 2ce83dd7ee14b14bf2b37e4d2d7382907ecd44d4
 
 
-    </div>
+
+
+  </div>
   )
-}
+  }  
+  
+
 
 export default Country
